b96fc31bcf7cdb98c6a81ee1d0415584994e9801
Hilton <hilton@asia.cybridge.jp>
2020-07-30 09:29:28 +0700

    first commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function _construct()
+    {
+      $this->middleware('auth');
+    }
+
+    public function create()
+    {
+    return view('post.create');
+    }
+    public function store(){
+      $data = request()->validate([
+        'title' => 'required',
+        'des' => 'required',
+        'category' => 'required',
+      ]);
+
+
+      
+
+      auth()->user()->posts()->create([
+        'title' => $data['title'],
+        'des' => $data['des'],
+        'category' => $data['category'],
+
+      ]);
+
+      return redirect('/profile/' . auth()->user()->id);
+
+    }
+    public function show(\App\Post $post)
+    {
+      return view('post.show',[
+        'post' => $post,
+      ]);
+    }
+}
